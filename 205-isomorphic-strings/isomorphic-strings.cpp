0096a04e03de190896c6dbdb39ca4d091d8b3372
sahilkhode48 <109568703+sahilkhode48@users.noreply.github.com>
2023-12-31 10:15:27 +0530

    Time: 6 ms (61.35%) | Memory: 7.3 MB (91.82%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,char>mpp;
-        map<char,char>mpp2;
+        int mapp[256] = {0};
+        bool ismapped[256]= {0};
         int n=s.size()-1;
+        //first we map and also set is mapped, if mapped already we dont change the mapp
         for(int i=0;i<=n;i++){
-            if(mpp[s[i]]!=mpp2[t[i]])return false;
-            mpp[s[i]]=i+1;
-            mpp2[t[i]]=i+1;
+            if(mapp[s[i]]==0 && (ismapped[t[i]]==0)){
+                mapp[s[i]]=t[i];
+                ismapped[t[i]]=true;
+
+            }
             
         }
+        //now we iterate and check if all are mapped;
 
+        for(int i=0;i<=n;i++){
+            if(char(mapp[s[i]])!= t[i]) return false;
+        }
         return true;
+
+        
     }
 };
